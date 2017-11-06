7a3c290d737df81e4a0412319a158d457b47ada9
tilaknshenoy <tilaknshenoy@gmail.com>
2017-11-06 16:18:14 +0530

    Assignment 5

--- lab6/prog5.c
+++ lab6/prog5.c
@@ -6,13 +6,15 @@ void main()
 int pid,pid2;
 int status;
 printf("Hello World!\n");
+int ppid=getppid();
 pid=fork();
+printf("%d\n",getppid());
 //printf("%d\n",pid);
 if(pid!=0)
         {
 
                 pid2=fork();
-//                printf("%d\n",pid2);
+                 printf("%d\n",getppid());
         }
 if(pid == -1) /* check for error in fork */
 {
@@ -21,19 +23,21 @@ exit(1);
 }
 if (pid == 0||pid2==0){
 
-if (pid==0)
+/*if (pid==0)
 printf("%d\n",pid);
 else 
-printf("%d\n",pid2);
+printf("%d\n",pid2);*/
 
 printf("I am the child process.\n");
 }
 else
 {
 wait(&status);
-printf("%d\n",pid);
-printf("%d\n",pid2);
+//printf("%d\n",pid);
+//printf("%d\n",pid2);
 printf("I am the parent process.\n");
+
+printf("%d\n",ppid);
 //printf("\n%d\n",pid);
 }
 }
